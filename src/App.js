1b82d510e158266028a3da6bf61ffea16c8430d2
Djourne <ceo@thavibes.com>
2024-03-10 00:18:56 -0500

    working so far

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Navbar />} />
           <Route path="/" element={<Landing_Page />} />
+          <Route path="/home" element={<Landing_Page />} />
+          <Route path="/signup" element={<Sign_Up />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
